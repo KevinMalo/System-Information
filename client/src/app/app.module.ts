6be36a5990aebec42cd7896379a1467854a8bd3f
Kevin Bueno <kean.solano@gmail.com>
2020-05-05 23:41:57 -0500

    Ram terminada

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { CpuTemperatureComponent } from './components/cpu-temperature/cpu-temperature.component';
+
+import { ChartsModule } from 'ng2-charts';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CpuTemperatureComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ChartsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
